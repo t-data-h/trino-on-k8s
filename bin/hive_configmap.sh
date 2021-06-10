fd8d77e00b41902e1d5971cf1debb75ff3914af9
tca <tcarland@gmail.com>
2021-06-10 06:47:11 -0700

    sync updates; add configmap

--- bin/hive_configmap.sh
+++ bin/hive_configmap.sh
@@ -3,16 +3,18 @@
 
 metacfg="conf/metastore-site.xml"
 corecfg="conf/core-site.xml"
+metatmp="metastore-site.xml"
+coretmp="core-site.xml"
 
 if [ -z "$MINIO_ENV_SH" ]; then
     . ~/.minio_env.sh
 fi
 
-HIVE_NS="${HIVE_NAMESPACE:-hive}"
-S3_ENDPOINT="${S3_ENDPOINT:-$(minio_endpoint)}"
-S3_ACCESS_KEY="${S3_ACCESS_KEY:-$(minio_accesskey)}"
-S3_SECRET_KEY="${S3_SECRET_KEY:-$(minio_secretkey)}"
-HIVE_USER="${HIVE_USER:-hive}"
+export HIVE_NS="${HIVE_NAMESPACE:-hive-metastore}"
+export S3_ENDPOINT="${S3_ENDPOINT:-$(minio_endpoint)}"
+export S3_ACCESS_KEY="${S3_ACCESS_KEY:-$(minio_accesskey)}"
+export S3_SECRET_KEY="${S3_SECRET_KEY:-$(minio_secretkey)}"
+export HIVE_USER="${HIVE_USER:-hive}"
 HIVE_PASSWORD="${HIVE_PASSWORD}"
 
 if [[ -z "$S3_ENDPOINT" || -z "$S3_ACCESS_KEY" || -z "$S3_SECRET_KEY" ]]; then
@@ -29,10 +31,7 @@ fi
 if [ -z "$HIVE_PASSWORD" ]; then
     HIVE_PASSWORD=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 8 | head -n 1)
 fi
-
-cfg_suffix=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
-metatmp="meta-site-${cfg_suffix}.xml"
-coretmp="core-site-${cfg_suffix}.xml"
+export HIVE_PASSWORD
 
 ( cat $metacfg | envsubst > $metatmp )
 ( cat $corecfg | envsubst > $coretmp )
