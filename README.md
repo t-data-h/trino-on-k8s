fa8bea3ec2306c4517de3d982c6f20bfc423a540
tca <tcarland@gmail.com>
2021-06-14 07:39:07 -0700

    readme updates use root db account keep tmpl in subdir

--- README.md
+++ README.md
@@ -2,4 +2,25 @@ Hive on Kubernetes
 ===================
 
 Kustomize and supporting scripts for running a Hive Metastore in Kubernetes using S3 
-and MySQL
+and MySQL. 
+
+
+## Deploying the Hive Metastore to K8s
+
+The project depends on a number of environment variables for deploying the 
+necessary configuration via a setup script. S3 Credentials are the primary 
+vars that are required, with others having default values if not provided.  The
+following table defines the list of variables used by the `./bin/setup.sh` script.
+
+| Environment Variable |    Description   |  Default Setting |
+| -------------------- | -------------------------------| ---------------|
+| S3_ENDPOINT          |  The S3 endpoint url |  n/a  |
+| S3_ACCESS_KEY        |  Corresponding access_key for S3 access | n/a |
+| S3_SECRET_KEY        |  The S3 secret key |  n/a  |
+| HIVE_NAMESPACE       |  Namespace for deploying the metastore | `hive-metastore`  |
+| MYSQLD_USER          |  Name of the hive mysql db user  | `hive` |
+| MYSQLD_ROOT_PASSWORD |  Password for the mysql root user |  *randomized password* |
+
+
+
+
--- bin/setup.sh
+++ bin/setup.sh
@@ -13,8 +13,8 @@ export HIVE_NS="${HIVE_NAMESPACE:-hive-metastore}"
 export S3_ENDPOINT="${S3_ENDPOINT:-$(minio_endpoint)}"
 export S3_ACCESS_KEY="${S3_ACCESS_KEY:-$(minio_accesskey)}"
 export S3_SECRET_KEY="${S3_SECRET_KEY:-$(minio_secretkey)}"
-export HIVE_USER="${HIVE_USER:-hive}"
-HIVE_PASSWORD="${HIVE_PASSWORD}"
+export MYSQLD_USER="${MYSQLD_USER:-root}"
+MYSQLD_ROOT_PASSWORD="${MYSQLD_ROOT_PASSWORD}"
 
 if [[ -z "$S3_ENDPOINT" || -z "$S3_ACCESS_KEY" || -z "$S3_SECRET_KEY" ]]; then
     echo "Error locating S3 credentials.."
@@ -23,19 +23,19 @@ fi
 
 if [[ ! -f conf/$metacfg || ! -f conf/$corecfg ]]; then
     echo "Error locating hive configurations in ./conf/ "
-    echo "  Ensure this script is run relative to project root"
+    echo " -> Ensure this script is run relative to project root"
     exit 1
 fi
 
-if [ -z "$HIVE_PASSWORD" ]; then
-    HIVE_PASSWORD=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 8 | head -n 1)
-    echo " -> HIVE_PASSWORD not set. Using auto-generated password: '$HIVE_PASSWORD'"
+if [ -z "$MYSQLD_ROOT_PASSWORD" ]; then
+    MYSQLD_ROOT_PASSWORD=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 8 | head -n 1)
+    echo " -> MYSQLD_ROOT_PASSWORD not set. Using auto-generated password: '$MYSQLD_ROOT_PASSWORD'"
 fi
-export HIVE_PASSWORD
+export MYSQLD_ROOT_PASSWORD
 
 ( cat conf/$metacfg | envsubst > hive-metastore/base/$metacfg )
 ( cat conf/$corecfg | envsubst > hive-metastore/base/$corecfg )
 
-( cat hive-initschema.tmpl | envsubst > hive-init-schema.yaml )
+( cat hive-metastore/hive-initschema.tmpl | envsubst > hive-init-schema.yaml )
 
 exit 0
