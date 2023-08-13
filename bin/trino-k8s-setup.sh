e37eb0e6163412deff341245c41fbba0bdd0cd5c
Timothy C. Arland <tcarland@users.noreply.github.com>
2023-08-13 07:33:29 -0600

    Tca 2308.13 (#57)
    * update hive image name and metastore deploymentb

--- bin/trino-k8s-setup.sh
+++ bin/trino-k8s-setup.sh
@@ -4,7 +4,7 @@
 #  source a secret.env with values needed.
 #
 PNAME=${0##*\/}
-VERSION="v23.08.12"
+VERSION="v23.08.13"
 
 metacfg="hive-site.xml"
 corecfg="core-site.xml"
@@ -21,7 +21,7 @@ components=("mysql-server" "hive-metastore" "trino")
 
 # -------------------------
 
-export HIVE_DEFAULT_IMAGE="quay.io/tcarland/hive:v3.1.3-metis-2304.19"
+export HIVE_DEFAULT_IMAGE="quay.io/tcarland/hive:v3.1.3-ersa-2308.12"
 export HIVE_IMAGE="${HIVE_IMAGE:-${HIVE_DEFAULT_IMAGE}}"
 
 export TRINO_NAMESPACE="${TRINO_NAMESPACE:-trino}"
