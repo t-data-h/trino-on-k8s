1580c2b4a66aad403ab2a72214246454875bde18
Timothy C. Arland <tcarland@users.noreply.github.com>
2026-05-16 10:13:38 -0600

    update label versions and hive image update ref (#150)

--- bin/trino-k8s-setup.sh
+++ bin/trino-k8s-setup.sh
@@ -4,7 +4,7 @@
 #  source a secret.env with values needed.
 #
 PNAME=${0##*\/}
-VERSION="v26.05.13"
+VERSION="v26.05.16"
 
 binpath=$(dirname "$0")
 project=$(dirname "$(realpath "$binpath")")
@@ -23,7 +23,7 @@ showenv=0
 
 # -------------------------
 
-export HIVE_DEFAULT_IMAGE="quay.io/tcarland/hive:v3.1.3-fenrir-2602.19"
+export HIVE_DEFAULT_IMAGE="quay.io/tcarland/hive:v3.1.3-ymir-2605.13"
 export HIVE_IMAGE="${HIVE_IMAGE:-${HIVE_DEFAULT_IMAGE}}"
 
 export TRINO_NAMESPACE="${TRINO_NAMESPACE:-${ns}}"
